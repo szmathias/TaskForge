f7e08079f9a4fc53984c7ce709244bfe177632f9
Zack Mathias <szmathias@gmail.com>
2025-12-17 18:48:33 -0500

    add item model and create_item endpoint to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, test!"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+async def read_item(item_id : int):
     return {"item_id": item_id}
 
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
