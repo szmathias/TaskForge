62b889717183618a5079a1de1809ad95e6d67987
Zack Mathias <szmathias@gmail.com>
2025-12-17 12:55:24 -0500

    add initial FastAPI setup with .gitignore and requirements

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def index():
+async def root():
     return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
 
