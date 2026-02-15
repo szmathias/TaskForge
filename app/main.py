383cc8c76ae968aa5bf55940a5c29e956c40fbab
Zack <szmathias@gmail.com>
2026-02-14 20:35:49 -0500

    add authentication router to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 import app.database as db
+from app.routers.auth import auth_router
 
 TaskForge = FastAPI()
+TaskForge.include_router(auth_router)
 
 db.Base.metadata.create_all(bind=db.engine)
 
